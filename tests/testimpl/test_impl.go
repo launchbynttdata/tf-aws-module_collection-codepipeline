d4018b7e99865ec27816c460b0cb5b8b31ca8f6e
Bryce Lynn <bryce.lynn@nttdata.com>
2024-06-11 13:52:50 -0700

    add working simple test for pipeline creation

--- tests/testimpl/test_impl.go
+++ tests/testimpl/test_impl.go
@@ -1,15 +1,36 @@
 package testimpl
 
 import (
+	"context"
 	"testing"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/codepipeline"
+	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/launchbynttdata/lcaf-component-terratest/types"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestComposableComplete(t *testing.T, ctx types.TestContext) {
-	t.Run("TestAlwaysSucceeds", func(t *testing.T) {
-		assert.Equal(t, "foo", "foo", "Should always be the same!")
-		assert.NotEqual(t, "foo", "bar", "Should never be the same!")
+func TestDoesPilelineExist(t *testing.T, ctx types.TestContext) {
+	pipelineId := terraform.OutputList(t, ctx.TerratestTerraformOptions(), "id")[0]
+	codepipelineClient := codepipeline.NewFromConfig(GetAWSConfig(t))
+
+	getPipelineOutput, err := codepipelineClient.GetPipeline(context.TODO(), &codepipeline.GetPipelineInput{Name: &pipelineId})
+	if err != nil {
+		t.Errorf("Error getting pipeline %s: %v", pipelineId, err)
+	}
+
+	t.Run("TestDoesPipelineExist", func(t *testing.T) {
+		require.Equal(t, pipelineId, *getPipelineOutput.Pipeline.Name, "Pipeline ID does not match")
+		require.NotEmpty(t, (*getPipelineOutput).Pipeline.Stages, "Pipeline does not have any stages")
 	})
+
+	//TODO: Add tests to verify that the pipeline runs successfully
+}
+
+func GetAWSConfig(t *testing.T) (cfg aws.Config) {
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	require.NoErrorf(t, err, "unable to load SDK config, %v", err)
+	return cfg
 }
